aeb635d557877fc46de9996126433749de2e2004
noegi <noe.gille76@gmail.com>
2024-09-01 18:35:51 +0200

    Added dependencies for python-for-android

--- get_credentials.py
+++ get_credentials.py
@@ -1,11 +1,5 @@
-from tgtg import TgtgClient
-
-client = TgtgClient(email="noe.gille76@gmail.com")
-credentials = client.get_credentials()
-file = open('token.txt', 'w')
-file.write('access_token:{}\n'.format(credentials['access_token']))
-file.write('refresh_token:{}\n'.format(credentials['refresh_token']))
-file.write('user_id:{}\n'.format(credentials['user_id']))
-file.write('cookie:{}\n'.format(credentials['cookie']))
-file.close()
-
+try:
+    import _ctypes
+    print("The _ctypes module is available.")
+except ModuleNotFoundError:
+    print("The _ctypes module is not available.")
